062ac707537fc00ab4b45e569b9e433273e096b7
Felipe1632 <felipesouzaribeiro24@gmail.com>
2026-05-05 14:32:05 -0300

    primeiro commit

---
+++ src/main/java/com/salao/barbearia/BarbeariaApplication.java
@@ -0,0 +1,13 @@
+package com.salao.barbearia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbeariaApplication.class, args);
+	}
+
+}
